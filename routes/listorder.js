d7d6dee6bb57da047d301ab227da73445ae98ea9
SanjanaCode <sanjanareddy.feb2001@gmail.com>
2021-11-11 16:35:38 -0800

    Finished Q1

--- routes/listorder.js
+++ routes/listorder.js
@@ -7,7 +7,8 @@ const moment = require('moment');
 
 router.get('/', function(req, res, next) {
     res.setHeader('Content-Type', 'text/html');
-    res.write('<title>YOUR NAME Grocery Order List</title>');
+    res.write('<title>ChaiMaMa Order List</title>');
+    res.write('<h1>Order List</h1>');
 
     /**
     Useful code for formatting currency:
@@ -33,24 +34,35 @@ router.get('/', function(req, res, next) {
     try {
         let pool = await sql.connect(dbConfig);
 
-        let sqlQuery1 = "SELECT o.orderId as ordId,o.orderDate,c.customerId,concat(firstName,' ',lastName) as name,totalAmount, p.productId, p.quantity, price FROM customer c join ordersummary o on c.customerId = o.customerId join orderproduct p on p.orderId = o.orderId";
+        let sqlQuery1 = "SELECT o.orderId as ordId,o.orderDate,c.customerId,concat(firstName,' ',lastName) as name,totalAmount FROM customer c join ordersummary o on c.customerId = o.customerId";
         let results1 = await pool.request()
             .query(sqlQuery1);
-        let sqlQuery2 = "SELECT count(*) from orderproduct";
-        let results2 = await pool.request()
-            .query(sqlQuery2);
-        res.write("<table><tr><th>Order Id</th><th>Order Date</th><th>Customer Id</th><th>Customer Name</th><th>Total Amount</th></tr>");
+        
+        res.write("<table border = \"1\"><tr><th>Order Id</th><th>Order Date</th><th>Customer Id</th><th>Customer Name</th><th>Total Amount</th></tr>");
         for (let i = 0; i < results1.recordset.length; i++) {
              let result1 = results1.recordset[i];
-             for(let j=0; j< result
-             if(result1.ordId == result)
-             res.write("<tr><td>" + result1.ordId + "</td><td>" + result1.orderDate + "</td><td>" + result1.customerId + "</td><td>" + result1.name + "</td><td>" + result1.totalAmount + "</td></tr>");
-        }
-    
-        res.write("</table>");
+             res.write("<tr><td>" + result1.ordId + "</td><td>" + moment(result1.orderDate).format('YYYY-mm-d HH:m:s') + "</td><td>" + result1.customerId + "</td><td>" + result1.name + "</td><td>" + result1.totalAmount + "</td></tr>");
+             res.write("<tr align= \"right\" ><td colspan= \"5\"><table border = \"1\"; ><tr><th>Product Id</th><th>Quantity</th><th>Price</th></tr>");
 
-        res.end();
-    } catch(err) {
+             let sqlQuery2 = "SELECT productId, quantity, price from orderproduct where orderId =" + result1.ordId;
+             let results2 = await pool.request()
+                .query(sqlQuery2);
+
+              for(let j=0; j <results2.recordset.length; j++){
+                let result2 = results2.recordset[j];
+                res.write("<tr><td>" + result2.productId + "</td><td>" + result2.quantity + "</td><td>" + result2.price + "</td></tr>");    
+            }
+
+             res.write("</table>");
+             res.write("</td></tr>");
+            }
+         
+            res.write("</table>");
+            res.end();
+        }
+        
+
+     catch(err) {
         console.dir(err);
         res.write(err)
         res.end();
