933664515f2217428e183739d06e9134e9a405b6
Thuan Vo <thuan.vo@ubc.ca>
2021-11-13 03:37:50 -0800

    Remove unused error classes

--- utilities/errors.js
+++ utilities/errors.js
@@ -12,18 +12,6 @@ class PropertyRequiredError extends ValidationError {
     }
 }
 
-class SQLError extends Error {
-    constructor(message) {
-        super(message);
-        this.name = this.constructor.name;
-    }
-}
-
-class ConnectionError extends SQLError {
-    constructor(message) {
-        super(message);
-    }
-}
 class OperationFailedError extends SQLError {
     constructor(message) {
         super(message);
@@ -33,7 +21,5 @@ class OperationFailedError extends SQLError {
 module.exports = {
     ValidationError,
     PropertyRequiredError,
-    SQLError,
-    ConnectionError,
     OperationFailedError
 };
