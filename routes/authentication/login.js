e0c0f3d5224d5a49e10a626f14b676883755f797
Thuan Vo <thuan.vo@ubc.ca>
2021-11-23 18:56:32 -0800

    Return login.html for /login route

--- routes/authentication/login.js
+++ routes/authentication/login.js
@@ -1,19 +1,8 @@
 const express = require('express');
 const router = express.Router();
-
-router.get('/', function(req, res, next) {
-    res.setHeader('Content-Type', 'text/html');
-    // Set the message for the login, if present
-    let loginMessage = false;
-    if (req.session.loginMessage) {
-        loginMessage = req.session.loginMessage;
-        req.session.loginMessage = false;
-    }
-
-    res.render('login', {
-        title: "Login Screen",
-        loginMessage: loginMessage
-    });
+const path = require('path');
+router.get('/', function (req, res, next) {
+    res.sendFile(path.join(__dirname, "../../public/layouts/login.html"));
 });
 
 module.exports = router;
