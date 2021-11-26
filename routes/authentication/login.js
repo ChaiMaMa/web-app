70d594bc14d00f8ae32001a3ab1b1a5ed171d1ca
Thuan Vo <thuan.vo@ubc.ca>
2021-11-26 03:45:43 -0800

    Redirect user back to main page if already authenticated

--- routes/authentication/login.js
+++ routes/authentication/login.js
@@ -2,7 +2,14 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 router.get('/', function (req, res, next) {
-    res.sendFile(path.join(__dirname, "../../public/layouts/login.html"));
+    // Check user is alraedy authenticated
+    if (req.session.user) {
+        console.log('User already authenticated.');
+        res.redirect('/');
+    } else {
+        res.sendFile(path.join(__dirname, "../../public/layouts/login.html"));
+    }
+
 });
 
 module.exports = router;
