dab3199c5375ed35d8262f4c390771db8b7b96bf
Thuan Vo <thuan.vo@ubc.ca>
2021-11-27 17:51:09 -0800

    Format html structure for clearer presentation

--- routes/products/listprod.js
+++ routes/products/listprod.js
@@ -35,15 +35,15 @@ router.get('/', async function (req, res, next) {
 
         productInfo += `
         <a href="/product?id=${product.productId}&name=${product.productName}&price=${product.productPrice}">
-            <div class="ref-product"><img class="ref-image"
-                        src=${src} loading="lazy" />
-                    <div class="ref-product-data">
-                        <div class="ref-product-info">
-                            <h5 class="ref-name">${product.productName}</h5>
-                        </div>
-                        <p class="ref-price">$${Number(product.productPrice).toFixed(2)}</p>
+            <div class="ref-product">
+                <img class="ref-image" src=${src} loading="lazy" />
+                <div class="ref-product-data">
+                    <div class="ref-product-info">
+                        <h5 class="ref-name">${product.productName}</h5>
                     </div>
+                    <p class="ref-price">$${Number(product.productPrice).toFixed(2)}</p>
                 </div>
+            </div>
         </a>
         `;
     }
