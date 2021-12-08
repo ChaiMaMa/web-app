5d7d58bda8d698c8b33195cdf89edf36f704edf8
Thuan Vo <thuan.vo@ubc.ca>
2021-12-08 01:21:40 -0800

    Add comments for clarifying authenticating steps

--- routes/authentication/validateLogin.js
+++ routes/authentication/validateLogin.js
@@ -15,12 +15,20 @@ router.post('/',
             console.log('Checking user...');
             // If authentication fails or database connection errors, user is consider not authenticated.
             try {
+                // Check system for the requested user with posted information
                 let authenticated = await isAuthenticated(req.body.username, hash(req.body.password));
+
+                // Return error if not authenticated (Check out http error codes 4xx!)
                 if (!authenticated) {
                     res.status(401).send('Authentication failed.');
                 } else {
+                    // Create an object representing the user
                     let user = new User(req.body.username, hash(req.body.password));
+
+                    // Add the user as a property to the current session after self-intializing
                     req.session.user = await user.intializeInfo();
+
+                    // Return success status
                     res.status(200).end();
                 }
             } catch (err) {
