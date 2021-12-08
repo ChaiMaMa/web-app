fbd6c6deeb25b23f5ab99f0495ff1ff7eba6ca5b
Thuan Vo <thuan.vo@ubc.ca>
2021-12-08 02:08:40 -0800

    Replace mock user with authenticated user info & add check for redirecting to login if not authenticated

--- routes/account/account.js
+++ routes/account/account.js
@@ -1,41 +1,54 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
+const { body } = require('express-validator');
 
-// Mock user
-let mockUser = {
-    customerId: 1,
-    firstName: "John",
-    lastName: "Smith",
-    email: "jsmith@gmail.com",
-    phoneNum: "2505551234",
-    address: "3174 University Way",
-    city: "Kelowna",
-    state: "BC",
-    postalCode: "V1V1V8",
-    country: "Canada"
-}
-
+// // Mock user used for testing
+// let mockUser = {
+//     customerId: 1,
+//     firstName: "John",
+//     lastName: "Smith",
+//     email: "jsmith@gmail.com",
+//     phoneNum: "2505551234",
+//     address: "3174 University Way",
+//     city: "Kelowna",
+//     state: "BC",
+//     postalCode: "V1V1V8",
+//     country: "Canada"
+// }
 
 router.get('/', function (req, res, next) {
 
-    let authenticated = mockUser != undefined;
-    res.setHeader('Content-Type', 'text/html');
-    res.render('layouts/account', {
-        title: `Your account - ${mockUser.firstName} ${mockUser.lastName}`,
-        auth_ref: authenticated ? "/account" : "/login",
-        auth_text: authenticated ? "Account" : "Login",
-        firstName: mockUser.firstName,
-        lastName: mockUser.lastName,
-        email: mockUser.email,
-        phoneNum: mockUser.phoneNum,
-        address: mockUser.address,
-        city: mockUser.city,
-        state: mockUser.state,
-        postalCode: mockUser.postalCode,
-        country: mockUser.country,
-        layout: false
-    });
+    // If the current session has a user property, we have an authenticated user.
+    let authenticated = req.session.user != undefined;
+
+    if (authenticated) {
+        // Define content type in http response message
+        res.setHeader('Content-Type', 'text/html');
+
+        let user = req.session.user;
+
+        console.log(user);
+
+        // Render the template
+        res.render('layouts/account', {
+            title: `Your account - ${user.info.firstName} ${user.info.lastName}`,
+            auth_ref: "#",
+            auth_text: "Account",
+            firstName: user.info.firstName,
+            lastName: user.info.lastName,
+            email: user.info.email,
+            phoneNum: user.info.phoneNum,
+            address: user.info.addressNum,
+            city: user.info.city,
+            state: user.info.state,
+            postalCode: user.info.postalCode,
+            country: user.info.country,
+            layout: false // This will not set a default layout (e.g. avoiding duplicate head/body tags)
+        });
+    } else { // Not authenticated? Redirect to login
+        res.redirect("/login");
+    }
 });
 
 module.exports = router;
