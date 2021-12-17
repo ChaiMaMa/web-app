5370b7b8e9ee4c3d5e53406c588e1b4ad49cdabd
Thuan Vo <thuan.vo@ubc.ca>
2021-12-17 11:07:22 -0800

    Fix undefined access error & Fix session property not updated after response is sent

--- routes/checkout/confirm.js
+++ routes/checkout/confirm.js
@@ -26,33 +26,28 @@ router.get("/:status/:hashedId", [
 
         // Hashed value saved in current session
         // If it is deleted, it is assigned as undefined.
-        let sessionHashedId = req.session.pendingOrder.hashedId;
-        console.log("Server says: " + sessionHashedId);
+        let pendingOrder = req.session.pendingOrder;
 
-        console.log(req.session);
 
-        if (reqHashedId == sessionHashedId) {
+        if (pendingOrder && reqHashedId == pendingOrder.hashedId) {
+            console.log("Server says: " + pendingOrder.hashedId);
             next();
         } else {
-            res.status(404).send();
+            res.redirect("/account");
         }
     },
     async function (req, res) {
         // Hashed value passed by client (browser)
         let status = req.params.status;
+
+        // Delete the pendingOrder
+        delete req.session.pendingOrder;
+
         if (status == "success") {
             res.sendFile(path.join(__dirname, '..', '..', 'public', 'layouts', 'success.html'));
         } else {
             res.sendFile(path.join(__dirname, '..', '..', 'public', 'layouts', 'error.html'));
         }
-
-        // Keep the session pendingOrder for 3s
-        await sleep(3000);
-
-        // After that remove it
-        console.log("Delete id!");
-        delete req.session.pendingOrder;
-        console.log(req.session);
     }]
 );
 
